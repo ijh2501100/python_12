f54ef21c5ec8347b072d8209ce64421460905fe5
DESKTOP-EL24N4K\annas <annas0801@gmail.com>
2025-11-26 11:17:01 +0900

    #36

---
+++ #36.py
@@ -0,0 +1,10 @@
+#36
+a = {100,200,300,400,500}
+b = {100,200,300,400,500}
+
+if a.issuperset(b) and a.issubset(b):
+    print('동시')
+elif a.issuperset(b):
+    print('상위')
+elif a.issubset(b):
+    print('부분')
